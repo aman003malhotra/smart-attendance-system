25fe494ef874fd7988df2984da2245310038df15
dell <anurag22012002@gmail.com>
2022-12-02 21:04:21 +0530

    frontend designing

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -49,125 +49,109 @@ router.get('/viewAttendenceStudent', ensureAuthenticated, ensureStudent, (req, r
     res.render('viewAttendenceStudent', {})
 });
 
+router.get('/aboutUs', (req, res) => {
+
+    res.render('aboutUs', {})
+});
 
 router.get('/teacherDashboard', ensureAuthenticated, ensureTeacher, (req, res) => {
-
-            router.get('/teacherDashboard', ensureAuthenticated, ensureTeacher, (req, res) => {
-                teacher_name = req.session.passport.user.name;
-                Subject.find({ subject_teacher: teacher_name })
-                    .then((teacher_data) => {
-                        // res.render('teacherDashboard', {teacher_data:teacher_data});
-                        res.render('teacherDashboard', { teacher_data: teacher_data });
-                        console.log(teacher_data)
-                    }).catch((err) => {
-                        console.log(err);
-                    })
+    teacher_name = req.session.passport.user.name;
+    Subject.find({ subject_teacher: teacher_name })
+        .then((teacher_data) => {
+            // res.render('teacherDashboard', {teacher_data:teacher_data});
+            res.render('teacherDashboard', { teacher_data: teacher_data });
+            console.log(teacher_data)
+        }).catch((err) => {
+            console.log(err);
+        })
 
 
-            });
 
-            router.get('/all-student', ensureAuthenticated, (req, res) => {
-                Student.find({})
-                    .then((students) => {
-                        res.render('allStudentsInfo', { students: students });
-                    }).catch((err) => {
-                        console.log(err);
-                    })
-            });
+});
 
+router.get('/all-student', ensureAuthenticated, (req, res) => {
+    Student.find({})
+        .then((students) => {
+            res.render('allStudentsInfo', { students: students });
+        }).catch((err) => {
+            console.log(err);
+        })
+});
 
-            router.get('/aboutUs', (req, res) => {
+router.get('/viewAttendenceTeacher', ensureAuthenticated, ensureTeacher, (req, res) => {
+    res.render('viewAttendenceTeacher', {})
+});
 
-                res.render('aboutUs', {})
-            });
+router.get('/markAttendence', ensureAuthenticated, ensureTeacher, (req, res) => {
+    res.render('markAttendence', {})
+});
 
+router.post('/foundStudents', ensureAuthenticated, ensureTeacher, (req, res, next) => {
+    store.set('student', req.body.number.number);
 
-            // router.get('/confirmation', (req, res) => {
-            //     console.log("Hello");
-            //     res.render('attendanceConfirmation', {})
-            // });
+});
 
-            router.get('/viewAttendenceTeacher', ensureAuthenticated, ensureTeacher, (req, res) => {
-
-                res.render('viewAttendenceTeacher', {})
-            });
-
-
-            router.get('/viewAttendenceTeacher', ensureAuthenticated, ensureTeacher, (req, res) => {
-                res.render('viewAttendenceTeacher', {})
-            });
-
-
-            router.get('/markAttendence', ensureAuthenticated, ensureTeacher, (req, res) => {
-                res.render('markAttendence', {})
-            });
-
-            router.post('/foundStudents', ensureAuthenticated, ensureTeacher, (req, res, next) => {
-                store.set('student', req.body.number.number);
-
-            });
-
-            router.get('/confirmation', ensureAuthenticated, ensureTeacher, async(req, res, next) => {
-                console.log("Redirecting");
-                console.log(store.get('student'));
-                list_of_student = store.get('student');
-                final_list = [];
-                if (list_of_student) {
-                    for (let i = 0; i < list_of_student.length; i++) {
-                        Student.find({ urn: list_of_student[i] })
-                            .then((user) => {
-                                final_list.push(user[0])
-                                if (final_list.length === list_of_student.length) {
-                                    console.log("final", final_list);
-                                    res.render('attendanceConfirmation', { final_list: final_list });
-                                }
-                            }).catch((err) => {
-                                console.log(err);
-                            });
+router.get('/confirmation', ensureAuthenticated, ensureTeacher, async(req, res, next) => {
+    console.log("Redirecting");
+    console.log(store.get('student'));
+    list_of_student = store.get('student');
+    final_list = [];
+    if (list_of_student) {
+        for (let i = 0; i < list_of_student.length; i++) {
+            Student.find({ urn: list_of_student[i] })
+                .then((user) => {
+                    final_list.push(user[0])
+                    if (final_list.length === list_of_student.length) {
+                        console.log("final", final_list);
+                        res.render('attendanceConfirmation', { final_list: final_list });
                     }
-                }
-            });
+                }).catch((err) => {
+                    console.log(err);
+                });
+        }
+    }
+});
 
 
 
-            router.post('/finalConfirmation', ensureAuthenticated, ensureTeacher, (req, res, next) => {
-                console.log("Final Confirmation");
-                console.log(store.get('student'));
-                const current_date = new Date();
-                list_of_student = store.get('student');
-                if (list_of_student) {
-                    for (let i = 0; i < list_of_student.length; i++) {
-                        Student.find({ urn: list_of_student[i] })
-                            .then((user) => {
-                                const newAttendance = new StudentAttendance({
-                                    teacherName: "kapil",
-                                    subject_code: user[0].subject_code,
-                                    studenturn: user[0].urn,
-                                    date: current_date,
-                                    slot: 2,
-                                    branch: user[0].branch
-                                });
+router.post('/finalConfirmation', ensureAuthenticated, ensureTeacher, (req, res, next) => {
+    console.log("Final Confirmation");
+    console.log(store.get('student'));
+    const current_date = new Date();
+    list_of_student = store.get('student');
+    if (list_of_student) {
+        for (let i = 0; i < list_of_student.length; i++) {
+            Student.find({ urn: list_of_student[i] })
+                .then((user) => {
+                    const newAttendance = new StudentAttendance({
+                        teacherName: "kapil",
+                        subject_code: user[0].subject_code,
+                        studenturn: user[0].urn,
+                        date: current_date,
+                        slot: 2,
+                        branch: user[0].branch
+                    });
 
-                                newAttendance
-                                    .save()
-                                    .then((data) => {
-                                        console.log("saved")
-                                    })
-                                    .catch(err => {
-                                        console.log(err)
-                                    })
-                            }).then(() => {
+                    newAttendance
+                        .save()
+                        .then((data) => {
+                            console.log("saved")
+                        })
+                        .catch(err => {
+                            console.log(err)
+                        })
+                }).then(() => {
 
-                                // ADDING REDIRECTION
+                    // ADDING REDIRECTION
 
-                                res.redirect('/markAttendence')
-                            }).catch((err) => {
-                                console.log(err);
-                            });
-                    }
-                }
-            });
+                    res.redirect('/markAttendence')
+                }).catch((err) => {
+                    console.log(err);
+                });
+        }
+    }
+});
 
 
 
-            module.exports = router;
+module.exports = router;
